c99b815edc87dfa230dce1dd25481ca7602ab365
JanMeyfarth1999 <jan_meyfarth@teilnehmer.cdemy.de>
2026-07-27 10:08:55 +0200

    Einbindung JS

---
+++ main.js
@@ -0,0 +1,9 @@
+// Hauptübeschrift 
+let headline = document.getElementById("main-headline");
+headline.textContent = "Kalender Datenblatt zum 21.07.2026";
+
+
+// Info zum aktuellen Tag
+let info = document.getElementById("info");
+info.textContent = "Der 21.07.2026 ist ein Dienstag, es ist nicht so warm wie die letzten Tage und es sind auch den rest der Woche keine hohen Temperaturen mehr zu erwarten.";
+
