e1ddc0519ec86a390dbec89cdac5bb1dd0d09577
JanMeyfarth1999 <jan_meyfarth@teilnehmer.cdemy.de>
2026-07-30 12:41:03 +0200

    Java

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
-// Hauptübeschrift 
-let headline = document.getElementById("main-headline");
-headline.textContent = "Kalender Datenblatt zum 21.07.2026";
+// Überschrift
+const heute = new Date();
+const datum = heute.toLocaleDateString("de-DE");
+ document.getElementById("ueberschrift").textContent =
+"Tagesbericht vom " + datum;
+// <p>
+//Wochentage
+const wochentage = [
+    "Sonntag",
+    "Montag",
+    "Dienstag",
+    "Mittwoch",
+    "Donnerstag",
+    "Freitag",
+    "Samstag",
+];
+const wochtag = wochentage[heute.getDay()];
+// Monate
+const Monate = [
+    "
+",
+
+   
+
+]
 
 
-// Info zum aktuellen Tag
-let info = document.getElementById("info");
-info.textContent = "Der 21.07.2026 ist ein Dienstag, es ist nicht so warm wie die letzten Tage und es sind auch den rest der Woche keine hohen Temperaturen mehr zu erwarten.";
+
 
 
