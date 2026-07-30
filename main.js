cc835ed27db047eb9ec15612bb6826fac6ecebd4
JanMeyfarth1999 <jan_meyfarth@teilnehmer.cdemy.de>
2026-07-30 14:46:12 +0200

    table

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 // Überschrift
 const heute = new Date();
 const datum = heute.toLocaleDateString("de-DE");
- document.getElementById("ueberschrift").textContent =
+ document.getElementById("main-headline").textContent =
 "Tagesbericht vom " + datum;
+
 // <p>
 //Wochentage
 const wochentage = [
@@ -14,7 +15,7 @@ const wochentage = [
     "Freitag",
     "Samstag",
 ];
-const wochtag = wochentage[heute.getDay()];
+const wochentag = wochentage[heute.getDay()];
 // Monate
 const Monate = [
     "Januar",
@@ -30,9 +31,42 @@ const Monate = [
     "November",
     "Dezember",
 ];
-const 
+const Monatsname = Monate[heute.getMonth()];
+// Jahr
+const Jahr = heute.getFullYear();
+//Tag
+const tag = heute.getDate();
+// Der wievielte "Wochentag(Mo,Di....)" im Monat ?
+const wiviwochentag = Math.ceil(tag / 7);
+// angezeigter Text im Web
+const Text =
+`Der ${datum} ist ein ${wochentag}, um genau zu sein der ${wiviwochentag}. ${wochentag} im Monat ${Monatsname} des Jahres ${Jahr}.`;
+document.getElementById("Tagesinfo").textContent = Text;
+
+//h2
+const tagMonat = heute.toLocaleDateString(
+    "de-DE",
+    {
+        day: "2-digit",
+        month: "2-digit"
+    }
+);
+document.getElementById("headline2").textContent = 
+"Historische Ereignisse am " + tagMonat;
+
+//Tabelle
+const tage = document.querySelectorAll("#kalender td");
+tage.forEach(function(zelle) {
+    
+
+    if (Number(zelle.textContent) ===Tag) {
+        zelle.classList.add("heute");
 
 
+    }
+
+
+    });
 
 
 
